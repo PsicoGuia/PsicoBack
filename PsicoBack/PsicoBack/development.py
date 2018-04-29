6f60afc736881c914c891d1aa495be0d37fe9eb1
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2018-04-29 11:47:19 -0500

    move to 2.0

--- PsicoBack/PsicoBack/development.py
+++ PsicoBack/PsicoBack/development.py
@@ -5,9 +5,11 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+import psycopg2
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'development',
         'USER': 'psico',
         'PASSWORD': 'ps1c0gu14',
@@ -17,9 +19,9 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
