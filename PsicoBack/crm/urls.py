79b175afc578743b2188d0a2f0aa0bc3bcf8e384
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2018-08-12 21:58:13 -0500

    update profile

--- PsicoBack/crm/urls.py
+++ PsicoBack/crm/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
         name="person-list"),
     url(r'^person/(?P<pk>[0-9]+)/$', views.PersonDetail.as_view(),
         name='person-detail'),
+    url(r'^user/$', views.UserList.as_view(),
+        name="user-list"),
+    url(r'^user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(),
+        name='user-detail'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 ]
