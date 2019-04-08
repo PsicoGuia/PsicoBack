a41923d5eaa564090c58fad01efc116a2bcb7822
Manuel Nuñez <manuelnt11@gmail.com>
2019-04-07 19:16:46 -0500

    Resuelto CORS

--- PsicoBack/PsicoBack/settings.py
+++ PsicoBack/PsicoBack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -178,9 +179,13 @@ POST_GIS_MAX_DISTANCE_SEARCH = 500
 CORS_ORIGIN_WHITELIST = (
     'psicoguia.co',
     'www.psicoguia.co'
+    'api.psicoguia.co'
 )
 CSRF_TRUSTED_ORIGINS = (
     'psicoguia.co',
     'www.psicoguia.co',
     'api.psicoguia.co'
+)
+CORS_ALLOW_HEADERS = default_headers + (
+    'access-control-allow-methods',
 )
